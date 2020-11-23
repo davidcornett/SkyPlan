676f3abdfbceff902e71e51b82d1fefa6705fed2
davidcornett <48662652+davidcornett@users.noreply.github.com>
2020-11-23 02:07:33 -0500

    Add files via upload
    auto-scroll functionality

--- carousel.js
+++ carousel.js
@@ -3,36 +3,61 @@
      https://medium.com/javascript-in-plain-english/writing-a-slider-in-
      javascript-learning-web-development-in-practice-ea4862dce85a
  */  
+document.addEventListener('DOMContentLoaded', carousel);
+
+function carousel() {
+
+    var autoScroll = true; //auto scroll unless user manually clicks thru carousel
+
+    let cardIndex = 1; //card immediately after starting card
+    showCards(cardIndex);
+
+    document.getElementById("leftCarousel").addEventListener("click", function() {
+        autoScroll = false; //turn off auto scroll
+        leftCard(); //initialize left button
+    });
+
+    document.getElementById("rightCarousel").addEventListener("click", function() {
+        autoScroll = false; //turn off auto scroll
+        rightCard(); //initialize right button
+    });
+
+    //auto-scroll to the right every 5 sec, but only until user clicks left or right
+    setInterval(function() {
+        if (autoScroll) {rightCard()};
+    }, 5000);
 
 
-let cardIndex = 1; //card immediately after starting card
-showCards(cardIndex);
-
-
-function nextCard() {
-    showCards(cardIndex += 1);  //moves index to right card
-}
-function previousCard() {
-    showCards(cardIndex -= 1); //moves index to left card
-}
-function currentCard(cur) {  //sets current card
-    showCards(cardIndex = cur); 
-}
-
-function showCards(n) {
-    let i;
-    let cards = document.getElementsByClassName("card");  //list of cards
-    
-    if (n > cards.length) {  //if past the last card, set index at 1
-      cardIndex = 1
+    function rightCard() {
+        showCards(cardIndex += 1);  //moves index to right card
     }
-    if (n < 1) {  //if to the left of the beginning card, set index to wrap around to the last card
-        cardIndex = cards.length
+    function leftCard() {
+        showCards(cardIndex -= 1); //moves index to left card
+    }
+    function currentCard(cur) {  //sets current card
+        showCards(cardIndex = cur); 
     }
 
-    for (let card of cards) {
-        card.style.display = "none";  //hide all cards
+    function showCards(n) {
+        let i;
+        let cards = document.getElementsByClassName("card");  //list of cards
+        
+        if (n > cards.length) {  //if past the last card, set index at 1
+          cardIndex = 1
+        }
+        if (n < 1) {  //if to the left of the beginning card, set index to wrap around to the last card
+            cardIndex = cards.length
+        }
+
+        for (let card of cards) {
+            card.style.display = "none";  //hide all cards
+        }
+       
+        cards[cardIndex - 1].style.display = "block";   //show current card
     }
-   
-    cards[cardIndex - 1].style.display = "block";   //show current card
+
+
 }
+
+
+
